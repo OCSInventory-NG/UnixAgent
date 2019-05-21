ee5bdf6620a2971a65a9d12c403f116aad8d84b6
Florian <lms23@web.de>
2019-05-21 16:39:44 +0200

    Pretty Print

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/NonLSB/SuSE.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/NonLSB/SuSE.pm
@@ -17,28 +17,28 @@ sub run {
     my $common = $params->{common};
 
     if(-s "/etc/os-release"){
-	   open V, "/etc/os-release" or warn;
-	    foreach (<V>) {
-        next if (/^#/);
+	open V, "/etc/os-release" or warn;
+	foreach (<V>) {
+        	next if (/^#/);
 		$osname=$1 if (/^PRETTY_NAME="([A-Z]*[a-z]*[0-9]*.*)"/);
-        $version=$1 if (/^VERSION_ID="([0-9]*.*)"/);
-        $patchlevel=$1 if (/^VERSION="([A-Z]*[0-9]*.*)"/);
+        	$version=$1 if (/^VERSION_ID="([0-9]*.*)"/);
+        	$patchlevel=$1 if (/^VERSION="([A-Z]*[0-9]*.*)"/);
         }
-       close V;
+        close V;
     }else{
-	   open V, "/etc/SuSE-release" or warn;
-	   $osname=<V>;
-	    foreach (<V>) {
-        next if (/^#/);
-        $version=$1 if (/^VERSION = ([0-9]+)/);
-        $patchlevel=$1 if (/^PATCHLEVEL = ([0-9]+)/);
+	open V, "/etc/SuSE-release" or warn;
+	$osname=<V>;
+	foreach (<V>) {
+        	next if (/^#/);
+        	$version=$1 if (/^VERSION = ([0-9]+)/);
+        	$patchlevel=$1 if (/^PATCHLEVEL = ([0-9]+)/);
         }
-       close V;
+        close V;
     }
 
     $common->setHardware({
         OSNAME => $osname,
-		OSVERSION => $version,
+	OSVERSION => $version,
         OSCOMMENTS => $patchlevel
     });
 }
