bb607c817e4a6657d490f7b2c7c5da61ac4503da
Florian <lms23@web.de>
2019-05-21 16:02:34 +0200

    Use of /etc/os-release or /etc/SuSE-release
    If exist, the client use the infos from /etc/os-release instead of /etc/SuSE-release
    Correct the "OSNAME" from /etc/SuSE-release (if no Enterprise Server)

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/NonLSB/SuSE.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/NonLSB/SuSE.pm
@@ -1,31 +1,44 @@
 package Ocsinventory::Agent::Backend::OS::Linux::Distro::NonLSB::SuSE;
 use strict;
 
-sub check { 
+sub check {
     my $params = shift;
     my $common = $params->{common};
-    $common->can_read ("/etc/SuSE-release") 
+    $common->can_read ("/etc/SuSE-release")
 }
 
 sub run {
     my $v;
     my $version;
     my $patchlevel;
-  
+    my $osname;
+
     my $params = shift;
     my $common = $params->{common};
-  
-    open V, "</etc/SuSE-release" or warn;
-    foreach (<V>) {
+
+    if(-s "/etc/os-release"){
+	   open V, "/etc/os-release" or warn;
+	    foreach (<V>) {
+        next if (/^#/);
+		$osname=$1 if (/^PRETTY_NAME="([A-Z]*[a-z]*[0-9]*.*)"/);
+        $version=$1 if (/^VERSION_ID="([0-9]*.*)"/);
+        $patchlevel=$1 if (/^VERSION="([A-Z]*[0-9]*.*)"/);
+        }
+       close V;
+    }else{
+	   open V, "/etc/SuSE-release" or warn;
+	   $osname=<V>;
+	    foreach (<V>) {
         next if (/^#/);
         $version=$1 if (/^VERSION = ([0-9]+)/);
         $patchlevel=$1 if (/^PATCHLEVEL = ([0-9]+)/);
+        }
+       close V;
     }
-    close V;
 
-    $common->setHardware({ 
-        OSNAME => "SUSE Linux Enterprise Server $version SP$patchlevel",
-        OSVERSION => $version,
+    $common->setHardware({
+        OSNAME => $osname,
+		OSVERSION => $version,
         OSCOMMENTS => $patchlevel
     });
 }
