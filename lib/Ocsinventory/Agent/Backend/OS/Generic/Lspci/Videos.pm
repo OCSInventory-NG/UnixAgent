8263e18f82fd0eb58a42173b5190f2079444d4e8
fbomj <frank.bourdeau@ocsinventory-ng.org>
2020-05-29 23:22:17 +0200

    Fix videos infos

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Lspci/Videos.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Lspci/Videos.pm
@@ -1,30 +1,37 @@
 package Ocsinventory::Agent::Backend::OS::Generic::Lspci::Videos;
 use strict;
-use warnings;
 
 my $memory;
-my $resolution;
 my $chipset;
 my $name;
-my $reso;
-
-#sub check {
-#    return unless $common->can_run("xrandr");
-#    return 1;
-#}
 
 sub run {
     my $params = shift;
     my $common = $params->{common};
 
-    foreach(`lspci`){
-        if(/graphics|vga|video|display/i && /^\S+\s([^:]+):\s*(.+?)(?:\(([^()]+)\))?$/i){
-            $common->addVideo({
-               'CHIPSET'  => $1,
-               'NAME'     => $2,
-            });
-        }
-    }
+   foreach(`lspci`){
+       if(/graphics|vga|video|display/i && /^([0-9a-f][0-9a-f]:[0-9a-f][0-9a-f].[0-9a-f])\s([^:]+):\s*(.+?)(?:\(([^()]+)\))?$/i){
+           my $slot = $1;
+           $chipset = $2;
+           $name = $3;
+           if (defined $slot) {
+               my @detail = `lspci -v -s $slot 2>/dev/null`;
+               $memory = 0;
+               foreach my $m (@detail) {
+                   if ($m =~ /.*Memory.*\s+\(.*-bit,\sprefetchable\)\s\[size=(\d*)M\]/) {
+                       $memory += $1;
+                   }
+               }
+               # Don't record zero memory
+               $memory = undef if $memory == 0;
+           }
+           $common->addVideo({
+               'CHIPSET'    => $chipset,
+               'NAME'       => $name,
+               'MEMORY'     => $memory,
+           });
+       }
+   }
 }
 
 1;
