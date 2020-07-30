909c409304dc7ba7311710af06667c5884fa9ef5
fbomj <frank.bourdeau@ocsinventory-ng.org>
2020-07-30 14:57:28 +0200

    Update CPU informations

--- lib/Ocsinventory/Agent/Backend/OS/MacOS/CPU.pm
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS/CPU.pm
@@ -16,12 +16,6 @@ sub run {
     my $arch;
     my $datawidth;
 
-    $processors->{1}->{MANUFACTURER} = `sysctl -n machdep.cpu.vendor`;
-    $processors->{1}->{TYPE} = `sysctl -n machdep.cpu.brand_string`;
-    $processors->{1}->{SPEED} = `sysctl -n hw.cpufrequency` / 1000 / 1000;
-    $processors->{1}->{L2CACHESIZE} = `sysctl -n hw.l2cachesize` / 1024;
-    $processors->{1}->{CORES} = `sysctl -n machdep.cpu.core_count`;
-    $processors->{1}->{LOGICAL_CPUS} = `sysctl -n machdep.cpu.thread_count`;
 
     # 32 or 64 bits arch?
     my $sysctl_arch = `sysctl -n hw.cpu64bit_capable`;
@@ -32,20 +26,32 @@ sub run {
        $arch = "x86";
        $datawidth = 32;
     }
-    $processors->{1}->{CPUARCH} = $arch;
-    $processors->{1}->{DATA_WIDTH} = $datawidth;
 
-    # copy cpu infos to other packages
+    # How much processor socket ?
     my $ncpu=`sysctl -n hw.packages`;
-    foreach my $cpu (2..$ncpu) {
-        $processors->{$cpu}->{MANUFACTURER} = $processors->{1}->{MANUFACTURER};
-        $processors->{$cpu}->{TYPE} = $processors->{1}->{TYPE};
-        $processors->{$cpu}->{SPEED} = $processors->{1}->{SPEED};
-        $processors->{$cpu}->{L2CACHESIZE} = $processors->{1}->{L2CACHESIZE};
-        $processors->{$cpu}->{CORES} = $processors->{1}->{CORES};
-        $processors->{$cpu}->{LOGICAL_CPUS} = $processors->{1}->{LOGICAL_CPUS};
-        $processors->{$cpu}->{CPUARCH} = $processors->{1}->{CPUARCH};
-        $processors->{$cpu}->{DATA_WIDTH} = $processors->{1}->{DATA_WIDTH};
+
+    # For each processor socket 
+    foreach my $cpu (1..$ncpu) {
+        $processors->{$cpu}->{MANUFACTURER} =  `sysctl -n machdep.cpu.vendor`;
+        if ($processors->{$cpu}->{MANUFACTURER} =~ /(Authentic|Genuine|)(.+)/i) {
+            $processors->{$cpu}->{MANUFACTURER} = $2;
+        }
+        chomp($processors->{$cpu}->{MANUFACTURER} );
+        $processors->{$cpu}->{TYPE} = `sysctl -n machdep.cpu.brand_string`;
+        chomp($processors->{$cpu}->{TYPE} );
+        $processors->{$cpu}->{SPEED} = `sysctl -n hw.cpufrequency` / 1000 / 1000;
+        $processors->{$cpu}->{L2CACHESIZE} = `sysctl -n hw.l2cachesize` / 1024;
+        $processors->{$cpu}->{CORES} = `sysctl -n machdep.cpu.core_count`;
+        chomp($processors->{$cpu}->{CORES});
+        $processors->{$cpu}->{THREADS} = `sysctl -n machdep.cpu.thread_count`;
+        chomp($processors->{$cpu}->{THREADS});
+        $processors->{$cpu}->{LOGICAL_CPUS} = `sysctl -n machdep.cpu.logical_per_package`;
+        chomp($processors->{$cpu}->{LOGICAL_CPUS});
+        $processors->{$cpu}->{CPUARCH} = $arch;
+        $processors->{$cpu}->{DATA_WIDTH} = $datawidth;
+        $processors->{$cpu}->{NBSOCKET} = $cpu;
+        chomp($processors->{$cpu}->{NBSOCKET});
+        $processors->{$cpu}->{SERIALNUMBER} = "N/A";
     }
     
     # Add new cpu infos to inventory
