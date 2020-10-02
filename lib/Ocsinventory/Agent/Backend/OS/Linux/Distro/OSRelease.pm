c6521dd30c555e72779fad7ff2fd9b22ee525915
Gilles Dubois <gilles.dubois@factorfx.com>
2020-10-02 14:41:23 +0200

    Add /etc/centos-release file management for exact version number

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/OSRelease.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Distro/OSRelease.pm
@@ -39,6 +39,16 @@ sub run {
         chomp($version);
     }
 
+    # CentOS exact version number is set in /etc/centos-release file
+    if (-r "/etc/centos-release") {
+        open V, "/etc/centos-release" or warn;
+        foreach (<V>) {
+            my $version = $1 if ($_ =~ /(\d+\.\d+)./g);
+        }
+        close V;
+        chomp($version);
+    }
+
     $common->setHardware({
         OSNAME => "$name $version",
         OSVERSION => $version,
