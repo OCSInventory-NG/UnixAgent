ad3f9c5f6a8123e77292b6b2daa7678c4a622c68
System Administrator <root@Mac-mini-de-Factorfx.local>
2022-04-20 16:50:45 +0200

    Fix domain name

--- lib/Ocsinventory/Agent/Backend/OS/MacOS/Domains.pm
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS/Domains.pm
@@ -4,45 +4,30 @@ use strict;
 # straight up theft from the other modules...
 
 sub check {
-    my $hostname;
-    chomp ($hostname = `hostname`);
-    my @domain = split (/\./, $hostname);
-    shift (@domain);
+    my $params = shift;
+    my $common = $params->{common};
+    return unless $common->can_run('dsconfigad');
+    my @domain = `dsconfigad -show`;
     return 1 if @domain;
-    -f "/etc/resolv.conf"
- }
+    0
+}
+
 sub run {
     my $params = shift;
     my $common = $params->{common};
 
     my $domain;
-    my $hostname;
-    chomp ($hostname = `hostname`);
-    my @domain = split (/\./, $hostname);
-    shift (@domain);
-    $domain = join ('.',@domain);
+    my $domainInfo;
+    chomp ($domainInfo = `dsconfigad -show`);
+    
+    my @domainInfo = split (/\n/, $domainInfo);
+    
+    shift(@domainInfo);
 
-    if (!$domain) {
-      my %domain;
-
-      open RESOLV, "/etc/resolv.conf" or warn;
-      while(<RESOLV>){
-        $domain{$2} = 1 if (/^(domain|search)\s+(.+)/);
-      }
-      close RESOLV;
-
-      $domain = join "/", keys %domain;
-    }
-
-    # If no domain name, we send "WORKGROUP"
-    $domain = 'WORKGROUP' unless $domain;
-
-    # User domain
-    my $userdomain = `defaults read /Library/Preferences/SystemConfiguration/com.apple.smb.server Workgroup 2>/dev/null`;
-    chomp($userdomain);
-    if ($userdomain eq "" ) {
-        $userdomain = `hostname -s`;
-        chomp($userdomain);
+    if($domainInfo[0]) {
+        @domainInfo = split(/\=/, $domainInfo[0]);
+        $domain = $domainInfo[1];
+        $domain =~ s/^\s+//;
     }
 
     $common->setHardware({
