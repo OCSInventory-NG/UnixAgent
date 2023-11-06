c1696a0afaa81abbf16670e79ecfa2cd254feea1
fbomj <frank.bourdeau@ocsinventory-ng.org>
2023-11-06 22:49:36 +0100

    Fix issue FreeBSD 13.2 with ocsinventory-agent 2.10.0,1 warning and missing hw.serialno

--- lib/Ocsinventory/Agent/Backend/OS/BSD/CPU.pm
+++ lib/Ocsinventory/Agent/Backend/OS/BSD/CPU.pm
@@ -1,52 +1,80 @@
 package Ocsinventory::Agent::Backend::OS::BSD::CPU;
 use strict;
+use warnings;
 
 sub check {
-    return unless -r "/dev/mem";
-    1;
+    my $params = shift;
+    my $common = $params->{common};
+    $common->can_run("lscpu");
 }
 
 sub run {
     my $params = shift;
     my $common = $params->{common};
 
-    my $os;
+    my @cpuinfos=$common->runcmd('lscpu');
+    my $cpu;
+    my $nbcpus;
 
-    my $processort;
-    my $processorn;
-    my $processors;
+    foreach my $info (@cpuinfos) {
+	chomp $info;
+	$cpu->{CPUARCH}=$1 if ($info =~ /Architecture:\s*(.*)/i);
+	$cpu->{NBCPUS}=$1 if ($info =~ /^CPU\(s\):\s*(\d+)/i);
+	$cpu->{CORES}=$1 if ($info =~ /Core\(s\)\sper\ssocket:\s*(\d+)/i);
+	$cpu->{NBSOCKET}=$1 if ($info =~ /Socket\(s\):\s*(.*)/i);
+	$cpu->{TYPE}=$1 if ($info =~ /Model\sname:\s*(.*)/i);
+	if ($info =~ /Vendor ID:\s*(Authentic|Genuine)(.+)/i){
+	    $cpu->{MANUFACTURER}=$2;
+	    $cpu->{MANUFACTURER}=~ s/(TMx86|TransmetaCPU)/Transmeta/;
+	    $cpu->{MANUFACTURER}=~ s/(CyrixInstead)/Cyrix/;
+	    $cpu->{MANUFACTURER}=~ s/(CentaurHauls)/VIA/;
+	}
 
-    my $family;
-    my $manufacturer;
-    my $serial;
-
-    chomp($os = `uname -s`);
-    if ($os eq "FreeBSD") {
-        $processors = `sysctl -n hw.clockrate`;
-    } else {
-        $processors = `sysctl -n hw.cpuspeed`;
+	$cpu->{CURRENT_SPEED}=$1 if ($info =~ /CPU\sMHz:\s*(\d+)(|\.\d+)$/i);
+	$cpu->{L2CACHESIZE}=$1 if ($info =~ /L2\scache:\s*(.*)/i);
+	if ($cpu->{CPUACRH} && $cpu->{CPUARCH} eq 'x86_64') {
+	    $cpu->{DATA_WIDTH}='64';
+	} else { 
+            $cpu->{DATA_WIDTH}='32';
+	}
+        if ($cpu->{TYPE}) {
+	    if ($cpu->{TYPE} =~ /([\d\.]+)MHz$/){
+		$cpu->{SPEED}=$1;
+	    } elsif ($cpu->{TYPE} =~ /([\d\.]+)GHz$/){
+	        $cpu->{SPEED}=$1*1000;
+            }
+        }
     }
-    $processorn = `sysctl -n hw.ncpu`;
-    $processort = `sysctl -n hw.model`;
 
-    $family = `sysctl -n hw.machine`;
-    $serial = `sysctl -n hw.serialno`;
+    # set CURRENT_ADDRESS_WIDTH with DATA_WIDTH value
+    $cpu->{CURRENT_ADDRESS_WIDTH}=$cpu->{DATA_WIDTH};
 
-    chomp($processort);
-    if ($processort =~ /Intel/) {
-        $manufacturer = "Intel";
+    my $infos=$common->getDmidecodeInfos();
+    foreach my $info (@{$infos->{4}}) {
+        next if $info->{Status} && $info->{Status} =~ /Unpopulated|Disabled/i;
+	$cpu->{SERIALNUMBER}=$info->{'Serial Number'};
+	$cpu->{THREADS}=$info->{'Thread Count'};
+	$cpu->{VOLTAGE}=$info->{'Voltage'};
+	$cpu->{SOCKET}=$info->{'Socket Description'};
     }
-    if ($processort =~ /Advanced Micro|AMD/) {
-        $manufacturer = "AMD";
+
+    # Set LOGICAL_CPUS with THREADS value;
+    $cpu->{LOGICAL_CPUS}=$cpu->{THREADS};
+
+    if ($cpu->{NBSOCKET} and $cpu->{CORES} and $cpu->{THREADS} ){
+        if ($cpu->{THREADS} >= $cpu->{CORES}) {
+            $cpu->{THREADS}=$cpu->{THREADS}/$cpu->{CORES};
+        } elsif ($cpu->{THREADS} >= $cpu->{NBSOCKETS}) {
+            $cpu->{THREADS}=$cpu->{THREADS}/$cpu->{NBSOCKETS};
+        }
     }
 
-    $common->addCPU({
-        CPUARCH => $family,
-        MANUFACTURER => $manufacturer,
-        CORES => $processorn,
-        TYPE => $processort,
-        SPEED => $processors,
-        SERIAL => $serial
-    });
+    for (my $i=0;$i<$cpu->{NBSOCKET};$i++) {
+        $common->addCPU($cpu);
+    }
+    
+    # Set the architecture
+    $common->setHardware({ ARCH => "$cpu->{CPUARCH}" });
+
 }
 1;
