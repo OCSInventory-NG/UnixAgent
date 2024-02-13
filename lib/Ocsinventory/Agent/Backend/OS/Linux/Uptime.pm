9e8750a6870f807d6cdd4dc8025c32574105b7d1
Djily SARR <djilysarr275@gmail.com>
2024-02-13 14:45:29 +0000

    Fix incorrect uptime retrieved from decription

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Uptime.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Uptime.pm
@@ -16,16 +16,35 @@ sub run {
     my $uptime = <UPTIME>;
     $uptime =~ s/^(.+)\s+.+/$1/;
     close UPTIME;
-  
-    # Uptime conversion
-    my ($UYEAR, $UMONTH , $UDAY, $UHOUR, $UMIN, $USEC) = (gmtime ($uptime))[5,4,3,2,1,0];
-  
-    # Write in ISO format
-    $uptime=sprintf "%02d-%02d-%02d %02d:%02d:%02d", ($UYEAR-70), $UMONTH, ($UDAY-1), $UHOUR, $UMIN, $USEC;
+
+    # Convert uptime 
+    my $uptime_converted = uptime_conversion($uptime);   
     
     chomp(my $DeviceType =`uname -m`);
     #  TODO$h->{'CONTENT'}{'HARDWARE'}{'DESCRIPTION'} = [ "$DeviceType/$uptime" ];
-    $common->setHardware({ DESCRIPTION => "$DeviceType/$uptime" });
+    $common->setHardware({ DESCRIPTION => "$DeviceType/$uptime_converted" });
+ 
+}
+
+sub uptime_conversion {
+    my ($uptime) = @_;
+
+    # Calculate current time
+    my $current_time = time();
+
+    # Calculate the time when the system was booted
+    my $boot_time = $current_time - $uptime;
+
+    # Convert boot time to human-readable format
+    my ($sec, $min, $hour, $mday, $mon, $year) = localtime($boot_time);
+    $year += 1900;
+    $mon += 1;
+    $sec += 1;
+
+    # Format the date and time
+    my $uptime_formated = sprintf("%02d-%02d-%04d %02d:%02d:%02d", $mday, $mon, $year, $hour, $min, $sec);
+
+    return $uptime_formated;
 }
 
 1
